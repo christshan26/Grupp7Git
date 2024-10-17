b8f643a953d3ac144673eec8cf3e1d31a0d88008
ALJI-1 <albin.ingelsson1@gmail.com>
2024-10-17 13:56:48 +0200

    Gjort färdigt funktionen "TaBortProdukt" och lagt till menyval.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,99 @@
-﻿namespace Grupp7Git
+﻿using System;
+
+using System.Collections.Generic;
+
+class Program
+
 {
-    internal class Program
+
+    static List<string> inventory = new List<string>();
+
+    static void Main(string[] args)
+
     {
-        static void Main(string[] args)
+
+        Console.WriteLine("Välkommen till Fabrikshanteringssystemet!");
+
+        while (true)
+
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("\nVälj en åtgärd:");
+
+            Console.WriteLine("1. Lägg till produkt");
+
+            Console.WriteLine("2. Visa inventarie");
+
+            Console.WriteLine("3. Ta bort produkt");
+
+            Console.WriteLine("3. Avsluta");
+
+
+
+            string val = Console.ReadLine();
+
+            switch (val)
+            {
+
+                case "1":
+
+                    LäggTillProdukt();
+
+                    break;
+                case "2":
+
+                    VisaInventarie();
+                    break;
+                case "3":
+
+                    TaBortProdukt();
+                    break;
+
+                case "4":
+
+                    return;
+
+                default:
+
+                    Console.WriteLine("Ogiltigt val. Försök igen.");
+
+                    break;
+
+            }
+
         }
+
+    }
+
+    static void LäggTillProdukt()
+
+    {
+
+        // TODO: Implementera metod för att lägga till produkt
+
+    }
+
+    static void VisaInventarie()
+
+    {
+
+
+    }
+    static void TaBortProdukt()
+    {
+        Console.WriteLine("Vilken produkt vill du ta bort?");
+        String? eraseProduct = Console.ReadLine();
+
+        if (inventory.Contains(eraseProduct))
+        {
+            inventory.Remove(eraseProduct);
+            Console.WriteLine($"{eraseProduct} har tagits bort");
+        }
+        else
+        {
+            Console.WriteLine($"{eraseProduct} finns inte i listan");
+        }
+        
     }
 }
+
