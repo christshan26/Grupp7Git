fac2e86833610a977317bf3966730d20af7227f7
christshan26 <christoffersten.hansson@gmail.com>
2024-10-17 13:12:02 +0200

    Initial commit med grundläggande kodskal

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace Grupp7Git
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    class Program
     {
+        static List<string> inventory = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till Fabrikshanteringssystemet!");
+            while (true)
+            {
+                Console.WriteLine("\nVälj en åtgärd:");
+                Console.WriteLine("1. Lägg till produkt");
+                Console.WriteLine("2. Visa inventarie");
+                Console.WriteLine("3. Avsluta");
+                string val = Console.ReadLine();
+                switch (val)
+                {
+                    case "1":
+                        LäggTillProdukt();
+                        break;
+                    case "2":
+                        VisaInventarie();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Ogiltigt val. Försök igen.");
+                        break;
+                }
+            }
+        }
+        static void LäggTillProdukt()
+        {
+            // TODO: Implementera metod för att lägga till produkt
+        }
+        static void VisaInventarie()
+        {
+            // TODO: Implementera metod för att visa inventarie
         }
     }
 }
